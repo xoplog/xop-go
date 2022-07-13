288bfcd2a1d6392510a86416d48f301d888927f4
David Muir Sharnoff <muir@idiom.org>
2022-07-12 22:27:52 -0700

    still WIP

--- span.go
+++ span.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/muir/xoplog/trace"
 	"github.com/muir/xoplog/xopconst"
+
+	"github.com/mohae/deepcopy"
 )
 
 type spanData struct {
@@ -54,36 +56,80 @@ func (l *Log) Span() *Span {
 	return &l.span
 }
 
-// Data adds one key/value pair to describe the span.  The
-// Span is returned so data methods can be chained.
-func (s *Span) Int64Data(key *xopconst.IntAttribute, value int64) *Span {
-	func() {
-		s.dataLock.Lock()
-		defer s.dataLock.Unlock()
-		s.data.intData = append(s.data.intData, intData{key: key, value: value})
-	}()
-	s.log.setDirty()
-	return s
-}
-func (s *Span) IntData(key *xopconst.IntAttribute, value int) *Span {
-	return s.Int64Data(key, int64(value))
-}
-func (s *Span) Int8Data(key *xopconst.IntAttribute, value int8) *Span {
-	return s.Int64Data(key, int64(value))
-}
-func (s *Span) Int16Data(key *xopconst.IntAttribute, value int16) *Span {
-	return s.Int64Data(key, int64(value))
-}
-func (s *Span) Int32ata(key *xopconst.IntAttribute, value int32) *Span {
-	return s.Int64Data(key, int64(value))
+func (s *Span) Data() *SpanData {
+	// TODO: PEROFMANCE: use a pool
+	return SpanDataSetter{
+		span: s,
+	}
 }
 
-func (s *Span) StrData(key *xopconst.StrAttribute, value string) *Span {
+type SpanData struct {
+	span *Span
+	data spanData
+}
+
+func (d *SpanData) Int(k *xopconst.IntAttribute, v int) *SpanData {
+	d.data.intData = append(d.data.intData, intData{key: k, value: int64(v)})
+	return d
+}
+
+func (d *SpanData) Int8(k *xopconst.IntAttribute, v int8) *SpanData {
+	d.data.intData = append(d.data.intData, intData{key: k, value: int64(v)})
+	return d
+}
+
+func (d *SpanData) Int16(k *xopconst.IntAttribute, v int16) *SpanData {
+	d.data.intData = append(d.data.intData, intData{key: k, value: int64(v)})
+	return d
+}
+
+func (d *SpanData) Int32(k *xopconst.IntAttribute, v int32) *SpanData {
+	d.data.intData = append(d.data.intData, intData{key: k, value: int64(v)})
+	return d
+}
+
+func (d *SpanData) Int64(k *xopconst.IntAttribute, v int64) *SpanData {
+	d.data.intData = append(d.data.intData, intData{key: k, value: v})
+	return d
+}
+
+func (d *SpanData) Str(k *xopconst.IntAttribute, v string) *SpanData {
+	d.data.strData = append(d.data.strData, strData{key: k, value: v})
+	return d
+}
+
+func (d *SpanData) Any(k *xopconst.IntAttribute, v interface{}) *SpanData {
+	v = deepcopy.Copy(v)
+	d.data.anyData = append(d.data.anyData, strData{key: k, value: v})
+	return d
+}
+
+func (d *SpanData) AnyImmutable(k *xopconst.IntAttribute, v interface{}) *SpanData {
+	d.data.anyData = append(d.data.anyData, strData{key: k, value: v})
+	return d
+}
+
+func (d *SpanData) Link(k *xopconst.LinkAttribute, v trace.Trace) *SpanData {
+	d.data.linkData = append(d.data.linkData, linkData{key: k, value: v})
+	return d
+}
+
+func (d *SpanData) Duration(k *xopconst.DurationAttribute, v time.Duration) *SpanData {
+	d.data.linkData = append(d.data.durationData, durationData{key: k, value: v})
+	return d
+}
+
+func (d *SpanData) Add() {
 	func() {
 		s.dataLock.Lock()
 		defer s.dataLock.Unlock()
-		s.data.strData = append(s.data.strData, intData{key: key, value: value})
+		// !GENERATE!
+		span.data.intData = append(span.data.intData, d.data.intData...)
+		span.data.strData = append(span.data.intData, d.data.strData...)
+		span.data.anyData = append(span.data.intData, d.data.anyData...)
+		span.data.timeData = append(span.data.intData, d.data.timeData...)
+		span.data.linkData = append(span.data.intData, d.data.linkData...)
+		span.data.durationData = append(span.data.intData, d.data.durationData...)
 	}()
 	s.log.setDirty()
-	return s
 }
