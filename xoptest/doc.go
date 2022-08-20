389859b4044be55ead503abe093939894b23141e
David Muir Sharnoff <muir@idiom.org>
2022-08-19 18:54:35 -0700

    add Events to xoptest

---
+++ xoptest/doc.go
@@ -0,0 +1,6 @@
+/*
+Package xoptest serves two purposes: it is a base logger you can
+use in tests and it is also provides a record of what got logged
+so that tests can introspect their own logs.
+*/
+package xoptest
