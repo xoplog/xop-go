e5a638c1f21a8b56205865d389f9193ca41a23cf
David Muir Sharnoff <muir@idiom.org>
2022-10-12 21:34:26 -0700

    xopjson no longer buffers internally. xopbytes has a richer API.
    Incompatible changes:
    
    - xopjson no longer supports WithBufferedLines()
    - xopbytes.BytesWriter interface has additional and changed methods
    
      xopbytes.WriteToIOWriter() is unchanged

--- xopbytes/bytelogger.go
+++ xopbytes/bytelogger.go
@@ -2,16 +2,41 @@
 package xopbytes
 
 import (
+	"time"
+
 	"github.com/xoplog/xop-go/trace"
+	"github.com/xoplog/xop-go/xopat"
+	"github.com/xoplog/xop-go/xopnum"
 )
 
 type BytesWriter interface {
 	Request(span trace.Bundle) BytesRequest
 	Buffered() bool
-	Close() // no point in returning an error
+	Close()                                      // no point in returning an error
+	DefineAttribute(*xopat.Attribute)            // duplicate calls should be ignored
+	DefineEnum(*xopat.EnumAttribute, xopat.Enum) // duplicate calls should be ignored
 }
 
 type BytesRequest interface {
 	Flush() error
-	Write([]byte) (int, error)
+	ReclaimMemory() // called when we know BytesRequest will never be used again
+	Span(Span, Buffer) error
+	Line(Line) error
+	AttributeReferenced(*xopat.Attribute) error
+}
+
+type Buffer interface {
+	AsBytes() []byte
+	ReclaimMemory()
+}
+
+type Line interface {
+	Buffer
+	GetSpanID() trace.HexBytes8
+	GetLevel() xopnum.Level
+	GetTime() time.Time
+}
+
+type Span interface {
+	GetSpanID() trace.HexBytes8
 }
