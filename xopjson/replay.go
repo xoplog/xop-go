82388cccae55ebf48a5d913b2a03ceb23e09faba
David Muir Sharnoff <github@dave.sharnoff.org>
2023-01-18 22:38:18 -0800

    WIP

---
+++ xopjson/replay.go
@@ -0,0 +1,13 @@
+package xopjson
+
+import (
+	"context"
+
+	"github.com/xoplog/xop-go/xopbase"
+
+	"github.com/pkg/errors"
+)
+
+func (logger *Logger) Replay(ctx context.Context, input any, output xopbase.Logger) error {
+	return errors.Errorf("TODO")
+}
