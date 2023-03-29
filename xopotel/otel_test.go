15ca5f2d76780709c3103eb187707618963901ab
David Muir Sharnoff <github@dave.sharnoff.org>
2023-03-28 21:24:19 -0700

    wip, now testing "SpanLog" and it does not work

--- xopotel/otel_test.go
+++ xopotel/otel_test.go
@@ -59,76 +59,117 @@ const jsonToo = false
 const otelToo = true
 
 func TestOTELBaseLoggerReplay(t *testing.T) {
-	for _, mc := range xoptestutil.MessageCases {
-		mc := mc
-		if mc.SkipOTEL {
-			continue
-		}
-		t.Run(mc.Name, func(t *testing.T) {
+	cases := []struct {
+		name       string
+		idGen      bool
+		baseLogger bool
+	}{
+		{
+			name:       "baselogger-with-id",
+			idGen:      true,
+			baseLogger: true,
+		},
+		{
+			name:       "baselogger-without-id",
+			idGen:      false,
+			baseLogger: true,
+		},
+		{
+			name:       "spanlog-no-id",
+			idGen:      false,
+			baseLogger: false,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			for _, mc := range xoptestutil.MessageCases {
+				mc := mc
+				if mc.SkipOTEL {
+					continue
+				}
+				t.Run(mc.Name, func(t *testing.T) {
 
-			var tpo []sdktrace.TracerProviderOption
+					var tpo []sdktrace.TracerProviderOption
 
-			var jBuffer xoputil.Buffer
-			if jsonToo {
-				jLog := xopjson.New(
-					xopbytes.WriteToIOWriter(&jBuffer),
-					xopjson.WithDuration("dur", xopjson.AsString),
-					xopjson.WithSpanTags(xopjson.SpanIDTagOption),
-					xopjson.WithAttributesObject(true),
-				)
+					var jBuffer xoputil.Buffer
+					if jsonToo {
+						jLog := xopjson.New(
+							xopbytes.WriteToIOWriter(&jBuffer),
+							xopjson.WithDuration("dur", xopjson.AsString),
+							xopjson.WithSpanTags(xopjson.SpanIDTagOption),
+							xopjson.WithAttributesObject(true),
+						)
 
-				jExporter := xopotel.NewExporter(jLog)
-				tpo = append(tpo, sdktrace.WithBatcher(jExporter))
+						jExporter := xopotel.NewExporter(jLog)
+						tpo = append(tpo, sdktrace.WithBatcher(jExporter))
+					}
+
+					rLog := xoptest.New(t)
+					rLog.SetPrefix("REPLAY ")
+					exporter := xopotel.NewExporter(rLog)
+					tpo = append(tpo, sdktrace.WithBatcher(exporter))
+
+					var buffer xoputil.Buffer
+					if otelToo {
+						otelExporter, err := stdouttrace.New(
+							stdouttrace.WithWriter(&buffer),
+							stdouttrace.WithPrettyPrint(),
+						)
+						require.NoError(t, err, "exporter")
+						tpo = append(tpo, sdktrace.WithBatcher(otelExporter))
+					}
+
+					if tc.idGen {
+						tpo = append(tpo, xopotel.IDGenerator())
+					}
+
+					tracerProvider := sdktrace.NewTracerProvider(tpo...)
+					ctx, cancel := context.WithCancel(context.Background())
+					defer func() {
+						err := tracerProvider.Shutdown(context.Background())
+						assert.NoError(t, err, "shutdown")
+					}()
+
+					tLog := xoptest.New(t)
+
+					if tc.baseLogger {
+						seed := xop.NewSeed(
+							xop.WithBase(tLog),
+							xopotel.BaseLogger(ctx, tracerProvider, true),
+						)
+						if len(mc.SeedMods) != 0 {
+							t.Logf("Applying %d extra seed mods", len(mc.SeedMods))
+							seed = seed.Copy(mc.SeedMods...)
+						}
+						log := seed.Request(t.Name())
+						mc.Do(t, log, tLog)
+
+					} else {
+						tracer := tracerProvider.Tracer("")
+						ctx, span := tracer.Start(ctx, mc.Name)
+						seed := xopotel.SpanLog(ctx, mc.Name).Span().SubSeed(xop.WithBase(tLog))
+						if len(mc.SeedMods) != 0 {
+							t.Logf("Applying %d extra seed mods", len(mc.SeedMods))
+							seed = seed.Copy(mc.SeedMods...)
+						}
+						log := seed.SubSpan("adding-tlog")
+						mc.Do(t, log, tLog)
+						span.End()
+					}
+					cancel()
+					tracerProvider.ForceFlush(context.Background())
+
+					if otelToo {
+						t.Log("logged:", buffer.String())
+					}
+					if jsonToo {
+						t.Log("Jlogged:", jBuffer.String())
+					}
+
+					t.Log("verify replay equals original")
+					xoptestutil.VerifyReplay(t, tLog, rLog)
+				})
 			}
-
-			rLog := xoptest.New(t)
-			rLog.SetPrefix("REPLAY ")
-			exporter := xopotel.NewExporter(rLog)
-			tpo = append(tpo, sdktrace.WithBatcher(exporter))
-
-			var buffer xoputil.Buffer
-			if otelToo {
-				otelExporter, err := stdouttrace.New(
-					stdouttrace.WithWriter(&buffer),
-					stdouttrace.WithPrettyPrint(),
-				)
-				require.NoError(t, err, "exporter")
-				tpo = append(tpo, sdktrace.WithBatcher(otelExporter))
-			}
-
-			// XXX test both with and without this
-			tpo = append(tpo, xopotel.IDGenerator())
-
-			tracerProvider := sdktrace.NewTracerProvider(tpo...)
-			ctx, cancel := context.WithCancel(context.Background())
-			defer func() {
-				err := tracerProvider.Shutdown(context.Background())
-				assert.NoError(t, err, "shutdown")
-			}()
-
-			tLog := xoptest.New(t)
-			seed := xop.NewSeed(
-				xop.WithBase(tLog),
-				xopotel.BaseLogger(ctx, tracerProvider, true),
-			)
-			if len(mc.SeedMods) != 0 {
-				t.Logf("Applying %d extra seed mods", len(mc.SeedMods))
-				seed = seed.Copy(mc.SeedMods...)
-			}
-			log := seed.Request(t.Name())
-			mc.Do(t, log, tLog)
-
-			cancel()
-			tracerProvider.ForceFlush(context.Background())
-			if otelToo {
-				t.Log("logged:", buffer.String())
-			}
-			if jsonToo {
-				t.Log("Jlogged:", jBuffer.String())
-			}
-
-			t.Log("verify replay equals original")
-			xoptestutil.VerifyReplay(t, tLog, rLog)
 		})
 	}
 }
